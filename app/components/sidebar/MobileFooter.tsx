a9c686fe27848e8aff1f705254cdf4043a8c0a8f
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-11-16 01:55:20 +0100

    glhf

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -3,21 +3,33 @@
 import useConversation from "@/app/hooks/useConversation"
 import useRoutes from "@/app/hooks/useRoutes"
 import MobileItem from "./MobileItem"
+import Avatar from "../Avatar"
+import { User } from "@prisma/client"
+import { useState } from "react"
+import SettingsModal from "./SettingsModal"
 
-const MobileFooter = () => {
+interface MobileFooterProps {
+    currentUser: User
+}
+
+const MobileFooter = ({
+    currentUser
+} : MobileFooterProps) => {
     const routes = useRoutes()
-    const { isOpen } = useConversation()
-
-    if (isOpen) {
-        return null
-    }
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div className="fixed justify-between w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden">
-            {routes.map((route) => (
-                <MobileItem key={route.label} href={route.href} active={route.active} icon={route.icon} onClick={route.onClick} />
-            ))}
-        </div>
+        <>
+            <SettingsModal currentUser={currentUser} isOpen={isOpen} onClose={() => setIsOpen(false)} />
+            <div className="fixed justify-between w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden">
+                {routes.map((route) => (
+                    <MobileItem key={route.label} href={route.href} active={route.active} icon={route.icon} onClick={route.onClick} />
+                ))}
+                    <div onClick={() => setIsOpen(true)} className="cursor-pointer mt-2 me-5 hover:opacity-75 transition">
+                        <Avatar user={currentUser} />
+                    </div>
+            </div>
+        </>
     )
 }
 
