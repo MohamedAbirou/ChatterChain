b56c81cfbcc1e199a354652f77b9a25f7372a2f0
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-11-17 15:18:10 +0100

    glhf

--- app/components/media-room-audio.tsx
+++ app/components/media-room-audio.tsx
@@ -1,18 +1,10 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import {
-    LiveKitRoom,
-    GridLayout,
-    ParticipantTile,
-    useTracks,
-    RoomAudioRenderer,
-    ControlBar,
-} from '@livekit/components-react';
+import { LiveKitRoom, VideoConference } from "@livekit/components-react"
 import "@livekit/components-styles"
 import { Loader2 } from "lucide-react"
 import { useSession } from "next-auth/react"
-import { Track } from "livekit-client";
 
 interface MediaRoomAudioProps {
     chatId: string
@@ -54,41 +46,10 @@ export const MediaRoomAudio = ({
     }
 
     return (
-        <LiveKitRoom
-          video={video}
-          audio={audio}
-          token={token}
-          serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
-          connect={true}
-          data-lk-theme="default"
-          style={{ height: '93%' }}
-        >
-          {/* Your custom component with basic video conferencing functionality. */}
-          <MyVideoConference />
-          {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
-          <RoomAudioRenderer />
-          {/* Controls for the user to start/stop audio, video, and screen 
-          share tracks and to leave the room. */}
-          <ControlBar />
-        </LiveKitRoom>
-      );
+        <div className="h-[90%]">
+            <LiveKitRoom data-lk-theme="default" serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL} token={token} connect={true} video={video} audio={audio}>
+                <VideoConference />
+            </LiveKitRoom>
+        </div>
+    )
 }
-
-function MyVideoConference() {
-    // `useTracks` returns all camera and screen share tracks. If a user
-    // joins without a published camera track, a placeholder track is returned.
-    const tracks = useTracks(
-      [
-        { source: Track.Source.Camera, withPlaceholder: true },
-        { source: Track.Source.ScreenShare, withPlaceholder: false },
-      ],
-      { onlySubscribed: false },
-    );
-    return (
-      <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
-        {/* The GridLayout accepts zero or one child. The child is used
-        as a template to render all passed in tracks. */}
-        <ParticipantTile />
-      </GridLayout>
-    );
-  }
