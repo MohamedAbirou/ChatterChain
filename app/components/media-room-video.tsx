331a2331bc20083e2bd7178456338fc678276dae
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-11-17 15:11:17 +0100

    glhf

--- app/components/media-room-video.tsx
+++ app/components/media-room-video.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { LiveKitRoom, VideoConference } from "@livekit/components-react"
+import { ControlBar, GridLayout, LiveKitRoom, ParticipantTile, RoomAudioRenderer, VideoConference, useTracks } from "@livekit/components-react"
 import "@livekit/components-styles"
 import { Loader2 } from "lucide-react"
 import { useSession } from "next-auth/react"
+import { Track } from "livekit-client"
 
 interface MediaRoomVideoProps {
     chatId: string
@@ -46,8 +47,41 @@ export const MediaRoomVideo = ({
     }
 
     return (
-        <LiveKitRoom data-lk-theme="default" className="h-[93%] sm:h-full" serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL} token={token} connect={true} video={video} audio={audio}>
-            <VideoConference />
+        <LiveKitRoom
+          video={video}
+          audio={audio}
+          token={token}
+          serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+          connect={true}
+          data-lk-theme="default"
+          style={{ height: '93%' }}
+        >
+          {/* Your custom component with basic video conferencing functionality. */}
+          <MyVideoConference />
+          {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
+          <RoomAudioRenderer />
+          {/* Controls for the user to start/stop audio, video, and screen 
+          share tracks and to leave the room. */}
+          <ControlBar />
         </LiveKitRoom>
-    )
+      );
 }
+
+function MyVideoConference() {
+    // `useTracks` returns all camera and screen share tracks. If a user
+    // joins without a published camera track, a placeholder track is returned.
+    const tracks = useTracks(
+      [
+        { source: Track.Source.Camera, withPlaceholder: true },
+        { source: Track.Source.ScreenShare, withPlaceholder: false },
+      ],
+      { onlySubscribed: false },
+    );
+    return (
+      <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+        {/* The GridLayout accepts zero or one child. The child is used
+        as a template to render all passed in tracks. */}
+        <ParticipantTile />
+      </GridLayout>
+    );
+  }
