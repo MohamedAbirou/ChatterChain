322c878d0bc20cf860751cc77538211e9c81cbea
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-11-17 19:09:33 +0100

    glhf

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,10 +3,10 @@ import AuthForm from "./components/AuthForm";
 
 export default function Home() {
     return (
-      <div className="flex min-h-full flex-col justify-center px-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="flex min-h-full flex-col justify-center my-3 md:my-0 px-12 sm:px-6 lg:px-8 bg-gray-100">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
             <Image alt="Logo" height="65" width="65" className="mx-auto w-auto" src="/images/logo.png" />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            <h2 className="mt-6 text-center text-xl sm:text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
         </div>
